aeee93129939005e3fdd009604b307815dc21c0f
rebvar-ebra <rebwar.ebrahimi@gmail.com>
2023-02-25 14:38:37 +0100

    event

--- src/component/Home/Home.jsx
+++ src/component/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Events from "./events/Events";
+import { dataApi } from './data'
 
 function Home() {
+    const [data,setData]=useState([])
+
+    useEffect(()=>{
+        const data = dataApi
+        setData(data);
+    },[])
   return (
-    <div>
-        
+    <div className="flex justify-center m-1 overflow-hidden" >
+        <Events  data={data}/>
     </div>
   )
 }
---
+++ src/component/Home/events/Events.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+import { dataApi } from '../data'
+function Events({data}) {
+    return (
+
+<div class="relative overflow-x-auto">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    id                
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Name
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Description
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Image
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Reviews
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Action
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+            {dataApi.map((item,i)=>(
+            <tr key={i} class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    {item.id}
+                    
+                </th>
+                <td class="px-6 py-4">
+                {item.name}
+
+                </td>
+                <td class="px-6 py-4">
+                    {item.description}
+                </td>
+                <td class="px-6 py-4">
+                   <img src= {item.imageBase64String} alt="" />
+                </td>
+                {/* <td class="px-6 py-4">
+                    {item.reviews}
+                </td> */}
+                <td class="px-6 py-4">
+                    <a href="#" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">Edit</a>
+                </td>
+
+                </tr>
+                ))}
+
+            
+        </tbody>
+    </table>
+</div>
+    )
+    }
+export default Events
